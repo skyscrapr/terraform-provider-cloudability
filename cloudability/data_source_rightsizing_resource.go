61961ff3f17e61f42eead3d16f6f76fc9a2ed168
Richard Weerasinghe <rich.w@me.com>
2020-05-16 18:08:30 +1200

    Closes #5

---
+++ cloudability/data_source_rightsizing_resource.go
@@ -0,0 +1,80 @@
+package cloudability
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/skyscrapr/cloudability-sdk-go/cloudability"
+	"log"
+)
+
+func dataSourceRightsizingResource() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceRightsizingResourceRead,
+		Schema: map[string]*schema.Schema{
+			"vendor": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Cloud Vendor",
+			},
+			"service": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Cloud Service",
+			},
+			"resource_identifier": {
+				Type:     schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Cloud instance id",
+			},
+			"default_instance_type": {
+				Type:     schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Default value for instance type",
+			},
+			"recommendations": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"action": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"nodeType": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"risk": {
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+					},
+				},
+				Description: "List of recommendation objects.",
+			},
+		},
+	}
+}
+
+func dataSourceRightsizingResourceRead(d *schema.ResourceData, meta interface{}) error {
+	vendor := d.Get("vendor").(string)
+	service := d.Get("service").(string)
+	resourceIdentifier := d.Get("resource_identifier").(string)
+
+	client := meta.(*cloudability.Client)
+	log.Printf("[DEBUG] resourceRightsizingResourceRead [resourceIdentifier: %q]", resourceIdentifier)
+	resource, err := client.Rightsizing().GetResource(vendor, service, resourceIdentifier)
+	// TODO: Handle default value for resource if the rightsizing instance or recommendation not found.
+	// Need a test for this too.
+	// Don't know what is returned if the instance does not exist.
+	if err != nil {
+		log.Printf("[DEBUG] Could not get Cloudability Rightsizing Resource: %q", err)
+		return err
+	}
+	d.SetId(resource.ResourceIdentifier)
+	d.Set("service", resource.Service)
+	return nil
+}
---
+++ cloudability/data_source_rightsizing_resource_test.go
@@ -0,0 +1,63 @@
+package cloudability
+
+import (
+	// 	"os"
+	"testing"
+	// "fmt"
+	// 	// 	"regexp"
+	// 	// 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	// "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	// "github.com/skyscrapr/cloudability-sdk-go/cloudability"
+	"net/url"
+	"net/http"
+	"net/http/httptest"
+)
+
+// func TestCloudabilityDataSourceRightsizingResource(t *testing.T) {
+// 	resource.Test(t, resource.TestCase{
+// 		IsUnitTest: true,
+// 		Providers:    testProviders,
+// 		CheckDestroy: testAccCheckCloudabilityDataSourceAccountVerificationDestroy,
+
+// 	})
+// }
+
+func TestDatasourceRightsizingResourceReadExistingRightsize(t *testing.T) {
+	vendor := "aws"
+	service := "ec2"
+	resourceIdentifier := "i-abcde12345"
+	resourceJSON := []byte(`{
+		"result": {
+			"service": "ec2",
+			"resourceIdentifier": "i-abcde12345",
+			"recommendations": [
+				{
+					"action": "Rightsize",
+					"nodeType": "x1e.xlarge",
+					"risk": 0
+				}
+			]
+		}
+	}`)
+	dataSource := dataSourceRightsizingResource()
+	d := dataSource.Data(nil)
+	d.Set("vendor", vendor)
+	d.Set("service", service)
+	d.Set("resource_identifier", resourceIdentifier)
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte(resourceJSON))
+	}))
+	defer testServer.Close()
+	config := Config{
+		APIKey: "MOCK",
+	}
+	client := config.Client()
+	client.V3BaseURL, _ = url.Parse(testServer.URL)
+
+	err := dataSourceRightsizingResourceRead(d, client)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
